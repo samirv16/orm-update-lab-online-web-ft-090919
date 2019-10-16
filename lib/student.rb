866b4718b7ff0849ec1cab9acca371d080e0dd7a
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 23:01:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,31 @@ class Student
   end 
   
   def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+
+end	  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end 
 
 
