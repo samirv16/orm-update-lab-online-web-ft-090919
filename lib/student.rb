df77bcf0b3c129b102aa16e68d443b9d096b6dac
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 23:11:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,9 +51,9 @@ class Student
 
   def self.new_from_db(row)
     student = self.new
-    id = row[0]
-    name = row[1]
-    grade = row[2]
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
     student
   end
   
