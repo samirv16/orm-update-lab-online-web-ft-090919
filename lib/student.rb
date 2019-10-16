22f04117ba479749f6204d543e7840f4015201aa
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 23:17:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
   
-  attr_accessor :name, :grade, :id 
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
     @name = name
