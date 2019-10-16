c7a45da0014056b18f046db9b3f18c348e5d88de
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 23:16:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,30 +1,33 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :id, :name, :grade
+  
+  attr_accessor :name, :grade, :id 
 
-
-  def initialize(id=nil, name, grade)
-    @id, @name, @grade = id, name, grade
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
   end
-
-
-
+  
   def self.create_table
-    sql = <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
-        name TEXT, 
+        name TEXT,
         grade TEXT
-      )
-    SQL
+        )
+        SQL
     DB[:conn].execute(sql)
   end
-
-  def self.drop_table 
-    DB[:conn].execute("DROP TABLE IF EXISTS students")
-  end
-
+  
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
   def save
     if self.id
       self.update
@@ -40,26 +43,33 @@ class Student
   end
 
 
-end	  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
-  end 
-
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+  
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE name = ?
+    SQL
     DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
-
+  
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+
 end
