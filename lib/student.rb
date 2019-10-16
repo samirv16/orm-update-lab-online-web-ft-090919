325ef4fede3a7430c365dba22229b0655c8dfe0c
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 22:59:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,22 @@ class Student
   end
   
   def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end 
 
 
 end
